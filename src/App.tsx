daca555073be8b945b51201eb9c126460137f602
Damageddream <marcan.grab@gmail.com>
2023-12-22 16:57:29 +0100

    connected with backend

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ type Message = string; // Replace 'any' with the actual type of your messages
 const App: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const ws = useRef<WebSocket | null>(null);
+  const [input, setInput] = useState<string>('');
 
   useEffect(() => {
     ws.current = new WebSocket('ws://localhost:8000/');
@@ -19,8 +20,28 @@ const App: React.FC = () => {
     };
   }, []);
 
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const input = event.currentTarget.elements[0] as HTMLInputElement;
+
+    if (input.value) {
+      ws.current?.send(input.value);
+      input.value = '';
+    }
+  }
+
+  const typeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setInput(event.currentTarget.value);
+  }
+
+
   return (
     <div>
+      <form onSubmit={submitHandler}>
+        <input onChange={typeHandler} value={input} type="text" />
+        <button type="submit">Send</button>
+      </form>
       {messages.map((message, index) => (
         <p key={index}>{message}</p>
       ))}
